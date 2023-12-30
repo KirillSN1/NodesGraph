79c3fa14d468b25d0588eb4ab1bd0d7e07cf7769
LENOVO-YOGA\kirill <kirill1234563456@gmail.com>
2023-12-30 19:56:12 +0300

    pinia deleted

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import { createPinia } from "pinia";
 // import store from "./store";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+createApp(App).use(router).mount("#app");
